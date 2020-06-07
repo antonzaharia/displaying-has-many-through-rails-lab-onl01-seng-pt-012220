0d95d322207e465b1373b893cb8f7818685a2ba8
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-07 11:56:09 +0000

    Automatically backed up by Learn

--- db/migrate/20200607105045_create_patients.rb
+++ db/migrate/20200607105045_create_patients.rb
@@ -3,6 +3,7 @@ class CreatePatients < ActiveRecord::Migration[5.0]
     create_table :patients do |t|
       t.string :name
       t.integer :age
+      t.has_many :appointments
 
       t.timestamps
     end
